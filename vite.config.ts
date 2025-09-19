7b1b6027565506b4e072347511e5b81ae2977ea5
kata671 <barbiebarbara480@gmail.com>
2025-09-19 19:02:36 +0200

    GH Pages: build to /docs with base=/skin-compass/

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 ﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
+  base: "/skin-compass/",
   plugins: [react()],
-  base: "/skin-scribe-suite/",   // <-- NAZWA REPO
-  build: { outDir: 'dist' }
+  build: { outDir: "docs" }
 });
-
